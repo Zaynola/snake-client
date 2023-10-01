8b38b78df3dfb6c8a71c2a2f4a9423e209838e2a
Zaynola <olaoluodushola@gmail.com>
2023-10-01 16:57:33 -0400

    create handleUserInput fnction

--- play.js
+++ play.js
@@ -1,2 +1,22 @@
 const connect = require("./client.js");
 console.log("Connecting ...");
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+
+    return stdin;
+  };
+
+  const handleUserInput = function (data) {
+    if (data === '\u0003') {
+        console.log("Exiting...");
+        process.exit();
+    }
+};
+const stdin = setupInput();
+stdin.on("data", handleUserInput);
